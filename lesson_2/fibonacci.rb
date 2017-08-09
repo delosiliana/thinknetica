943bfed3f41a273d7c6c83e4d35640fc0bb1b1a9
delosiliana <dronixa@mail.ru>
2017-08-09 12:14:53 +0300

    done fibonacci

--- lesson_2/fibonacci.rb
+++ lesson_2/fibonacci.rb
@@ -1,3 +1,18 @@
 =begin
 Заполнить массив числами фибоначчи до 100
 =end
+def fibonacci
+	fib = []
+	fib[1] , fib[0] = 1 , 0
+
+	n = 1
+	while n < 100 do
+		n+=1
+		i = fib[n-2] + fib[n-1]
+		fib << i
+	end
+	fib.delete_if {|i| i > 100}
+end
+
+p fibonacci
+
