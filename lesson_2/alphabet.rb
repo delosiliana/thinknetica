50e449863f2551235d2a06ee2d95c092ed52e1da
delosiliana <dronixa@mail.ru>
2017-08-09 12:37:16 +0300

    done task

--- lesson_2/alphabet.rb
+++ lesson_2/alphabet.rb
@@ -1,3 +1,13 @@
 =begin
 Заполнить хеш гласными буквами, где значением будет являтся порядковый номер буквы в алфавите (a - 1).
 =end
+def alphabet
+	letters = ('a'..'z').to_a
+	letters_array = ['a', 'e', 'i', 'o', 'u', 'y']
+	letters_hash ={}
+
+	letters.each_with_index{ |v, i| letters_hash[v] = i+1 if letters_array.include? v}
+	puts letters_hash
+end
+
+p alphabet
