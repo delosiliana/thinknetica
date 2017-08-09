3ed274c950923d532c4f4f779b710a2fc3645963
delosiliana <dronixa@mail.ru>
2017-08-09 22:02:40 +0300

    Fixed tab

--- lesson_2/basket.rb
+++ lesson_2/basket.rb
@@ -7,39 +7,39 @@
 Вычислить и вывести на экран итоговую сумму всех покупок в "корзине".
 =end
 def basket
-	full_n = []
-	full_p = []
-	number_of = []
-	full_price = []
-	name = nil
+  full_n = []
+  full_p = []
+  number_of = []
+  full_price = []
+  name = nil
 
-	while name != "stop" do
-		puts "Enter product name or stop"
-		name = gets.chomp
-		full_n << name
+  while name != "stop" do
+    puts "Enter product name or stop"
+    name = gets.chomp
+    full_n << name
 
-		if name != "stop"
-		puts "Enter price product?"
-		price = gets.chomp.to_f
-		full_p << price
+    if name != "stop"
+    puts "Enter price product?"
+    price = gets.chomp.to_f
+    full_p << price
 
-		puts "how much  a product you have purchased?"
-		number = gets.chomp.to_f
-		number_of << number
+    puts "how much  a product you have purchased?"
+    number = gets.chomp.to_f
+    number_of << number
 
-		full_price << price * number
-	end
-		full_n.delete("stop")
-	end
+    full_price << price * number
+  end
+    full_n.delete("stop")
+  end
 
-	sum = 0
-	full_price.each{|a| sum +=a}
+  sum = 0
+  full_price.each{|a| sum +=a}
 
-	the_cost = Hash[full_p.zip number_of]
-	full_purchase = Hash[full_n.zip the_cost]
+  the_cost = Hash[full_p.zip number_of]
+  full_purchase = Hash[full_n.zip the_cost]
 
-	puts full_purchase
-	puts "Total amount is: #{sum}"
+  puts full_purchase
+  puts "Total amount is: #{sum}"
 end
 
 p basket
--- lesson_2/date.rb
+++ lesson_2/date.rb
@@ -6,31 +6,31 @@
 
 def day_date
 
-	puts "Введите число месяца"
-	day = gets.chomp.to_i
+  puts "Введите число месяца"
+  day = gets.chomp.to_i
 
-	puts "Введите месяц"
-	month = gets.chomp.to_i
+  puts "Введите месяц"
+  month = gets.chomp.to_i
 
-	puts "Введите год"
-	year = gets.chomp.to_i
+  puts "Введите год"
+  year = gets.chomp.to_i
 
-	month_arr = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-	if year % 4 == 0 && year % 100 !=0 || year % 400 == 0
-		month_arr[1] = 29
-	end
+  month_arr = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+  if year % 4 == 0 && year % 100 !=0 || year % 400 == 0
+    month_arr[1] = 29
+  end
 
-	num = 0
-	y = 0
+  num = 0
+  y = 0
 
-	while y < month
-		num += month_arr[y]
-		y +=1
-	end
+  while y < month
+    num += month_arr[y]
+    y +=1
+  end
 
-	num+=day
+  num+=day
 
-	puts "Прошло с начала года #{day + num} дней"
+  puts "Прошло с начала года #{day + num} дней"
 end
 
 p day_date
