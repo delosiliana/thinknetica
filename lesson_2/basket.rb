93b3a5e5faf4f8db501b219facdabecc59739d72
delosiliana <dronixa@mail.ru>
2017-08-09 15:56:37 +0300

    Basket task done

--- lesson_2/basket.rb
+++ lesson_2/basket.rb
@@ -6,3 +6,40 @@
 единицу товара и кол-во купленного товара. Также вывести итоговую сумму за каждый товар.
 Вычислить и вывести на экран итоговую сумму всех покупок в "корзине".
 =end
+def basket
+	full_n = []
+	full_p = []
+	number_of = []
+	full_price = []
+	name = nil
+
+	while name != "stop" do
+		puts "Enter product name or stop"
+		name = gets.chomp
+		full_n << name
+
+		if name != "stop"
+		puts "Enter price product?"
+		price = gets.chomp.to_f
+		full_p << price
+
+		puts "how much  a product you have purchased?"
+		number = gets.chomp.to_f
+		number_of << number
+
+		full_price << price * number
+	end
+		full_n.delete("stop")
+	end
+
+	sum = 0
+	full_price.each{|a| sum +=a}
+
+	the_cost = Hash[full_p.zip number_of]
+	full_purchase = Hash[full_n.zip the_cost]
+
+	puts full_purchase
+	puts "Total amount is: #{sum}"
+end
+
+p basket
