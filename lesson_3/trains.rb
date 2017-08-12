39b9ff87e2e5b5f8128ede3206e94f0a6e149d19
delosiliana <dronixa@mail.ru>
2017-08-12 06:41:08 +0300

    Add Station option

--- lesson_3/trains.rb
+++ lesson_3/trains.rb
@@ -1,39 +1,45 @@
 class Station
-attr_accessor :train, :name, :station
+  attr_accessor :train, :name, :station
 
-  def initialize(name)            #названия станции
+  def initialize(name) # названия станции
     @name = name
     @all_trains = []
   end
 
-  def taking_trained               #принимает поезда
-
+  def arrived(train) # принимает поезда
+    all_trains << train
+    puts "На станцию #{name} прибывает поезд #{train}"
   end
 
-  def list_trains                      #список поездов
-
+  def station_info # список поездов
+    puts "Всего #{@all.trains.size} поездов на станции #{@name}"
   end
 
-  def send_train                     #отправить поезд
+  def depart(train) # отправить поезд
+    all_trains.delete(train)
+    puts "Поезд #{train} уходит со станции #{name}"
+  end
 
+  def train_by_type
+    puts "Грузовых поездов: #{(@all_trains.select {|train| all_train[ :type] == "cargo"}).size}"
+    puts "Пассажирских поездов: #{(@all_trains.select {|train| all_train[ :type] == "passenger"}).size}"
   end
 end
 
 class Route
-
-  def name_city                       #Имена городов отправления и прибытия
-
-  end
-
-  def add_intermediate     #Промежуточные станции
+  def name_city
 
   end
 
-  def  delete_intermediate     #удалить промежуточную станцию
+  def add_intermediate
 
   end
 
-  def list_city_station            #список всех станций
+  def delete_intermediate
+
+  end
+
+  def list_city_station
 
   end
 end
@@ -41,40 +47,40 @@ end
 class Train
   attr_reader :number, :type, :carriages
 
-  def initialize(opt ={})                                        #создает номер, тип(пассажирский, грузовой),и количество вагонов
-    @number = opts[:number]
-    @type = opts[:type]
-    @carriages = opts[:carriages].to_i
+  def initialize(number, type, carriages) # создает номер, тип(пассажирский, грузовой),и количество вагонов
+    @number = number
+    @type = type.to_s
+    @carriages = carriages.to_i
     stop
   end
 
-  def increments_speed                                   # может набирать скорость, возвращать текуюую, тормозить.
-    @initial_speed +=10
+  def increments_speed # может набирать скорость, возвращать текуюую, тормозить.
+    @initial_speed += 10
   end
 
   def reset_speed
     stop
   end
 
-  def carriage(option)                #прицепить\отцепить(если поезд стоит)
+  def carriage(option) # прицепить\отцепить(если поезд стоит)
     reset_speed
-    @carriages +=1 if option == 'attach'
-    @carriages -=1 if option == 'unhook'
+    @carriages += 1 if option == 'attach'
+    @carriages -= 1 if option == 'unhook'
   end
 
-  def list_the_car                          #показать количество вагонов
+  def list_the_car # показать количество вагонов
     puts "Количество вагонов в поезде: #{carriages}"
   end
 
-  def route_train                             #может принимать маршрут следования, при назначении автоматически находится на начальной станции
+  def route_train
 
   end
 
-  def move_station                               #перемещение между станциями, вперед-назад но только на 1 станцию
+  def move_station
 
   end
 
-  def list_train_station                         #показать, текущую, предыдущую и следущую станцию
+  def list_train_station
 
   end
 
@@ -84,3 +90,10 @@ class Train
     @initial_speed = 0
   end
 end
+
+station1 = Station.new("Moscow")
+train1 = Train.new(120, 'cargo', 10)
+train2 = Train.new(21,'passenger', 20)
+train2.list_the_car
+train2.carriage('attach')
+train2.list_the_car
