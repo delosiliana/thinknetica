9fbb7fd2e87c355b8709925f60cb0651d0eb9902
delosiliana <dronixa@mail.ru>
2017-08-15 06:25:40 +0300

    Fix 3

--- lesson_3/trains.rb
+++ lesson_3/trains.rb
@@ -1,5 +1,5 @@
 class Station
-  attr_accessor :name
+  attr_accessor :name, :trains
 
   def initialize(name)  # названия станции
     @trains = []
@@ -7,18 +7,17 @@ class Station
 
   def add_train(train)  # принимает поезда
     @trains << train
-    puts "На станцию #{name} прибывает поезд #{train}"
+    @trains.join(", ")
   end
 
-  def trains   #список поездов
-    puts "Всего #{@trains.size} поездов на станции #{@name}"
+  def by_type(type)
+    @trains.group_by(&:type).each { |key, value| print "#{key}: #{value.count} " if key == type }
   end
 
   def depart(train)  # отправить поезд
     trains.delete(train)
-    puts " Поезд #{train} уходит со станции #{name} "
   end
- end
+end
 
 class Route
   attr_reader :stations
@@ -29,27 +28,23 @@ class Route
 
   def add_station(station)    #добавление станции в маршрут
     @stations.insert(-2, station)
-    puts "Станция (#{station}) добавлена в маршрут #{@number}."
   end
 
   def delete_station(station)    #удаление станции из маршрута
     if station != @stations.first && station != @stations.last
       @stations.delete(station)
-      puts "Станция (#{station}) удалена из маршрута #{@number}."
     end
   end
 end
 
 class Train
   attr_reader :number, :type, :carriages
-  attr_accessor :route
+  attr_accessor :number, :type, :carriages, :route
 
-  def initialize(number, type, carriages)   # создает номер, тип(пассажирский, грузовой),и количество вагонов
-    @number = number
-    @type = type.to_sym
-    @carriages = carriages.to_i
-    stop
-    puts "Создан поезд №#{number} состав: #{type}, c #{carriages} вагонами"
+  def initialize(opts = {})
+    @carriages = opts[:carriages]
+    @number    = opts[:number]
+    @type      = opts[:type]
   end
 
   def accelerate  # может набирать скорость, возвращать текуюую, тормозить.
@@ -63,15 +58,11 @@ class Train
   def add_carriage   # прицепить(если поезд стоит)
     stop
     @carriages += 1
-    puts "Прицеплен вагон, на данный момент кол-во вагонов составляет #{carriages}"
   end
 
   def remove_carriage  #отцепить(если поезд стоит)
     stop
-    unless @carriages <= 1
-      puts "Отцеплен вагон, на данный момент кол-во вагонов составляет #{@carriages -= 1}"
-    else puts "У вас всего 1 вагон, вам нечего отцеплять"
-    end
+    @carriages -= 1 if @carriages > 0
   end
 
   def route_train(route)
@@ -81,48 +72,49 @@ class Train
 
   def move_next
     @st_index += 1 if @route.stations[@st_index + 1]
-    puts "Вы прибыли на станцию #{@route.stations[@st_index]}"
   end
 
   def move_previous
     @st_index -= 1 if @st_index > 0
-    puts "Вы прибыли на станцию #{@route.stations[@st_index]}"
   end
 
   def current_station
     current = @route.stations[@st_index]
-    puts "Сейчас вы находитесь на станции #{current}"
   end
 
   def next_station
     @next_st = @route.stations[@st_index + 1]
-    puts "Следующая станция по вашему маршруту #{@next_st}"
   end
 
   def previous_station
     @pr_st = @route.stations[@st_index - 1] if @st_index > 0
-    puts "Предыдущая станция по вашему маршруту: #{@pr_st}"
   end
 end
 
-st1= Station.new('Moscow')
-t1 = Train.new('91', 'passenger', '16')
+stans= Station.new('Moscow')
+one = Train.new(number: "91", type: "passenger", carriages: 15)
+two = Train.new(number: "24", type: "passenger", carriages: 38)
+three = Train.new(number: "16", type: "cargo", carriages: 21)
 route1 = Route.new('Moscow', 'Dnepr')
 route1.add_station('Tula')
 route1.add_station('Belgorod')
 route1.add_station('Harkov')
-t1.route_train(route1)
-t1.current_station
-t1.next_station
-t1.move_next
-t1.move_next
-t1.move_previous
-t1.current_station
-t1.next_station
-t1.previous_station
-t1.remove_carriage
-t1.remove_carriage
-t1.move_previous
-t1.current_station
-st1.add_train('t1')
-st1.trains
+one.route_train(route1)
+one.current_station
+one.next_station
+one.move_next
+one.move_next
+one.move_previous
+one.current_station
+one.next_station
+one.previous_station
+one.remove_carriage
+one.remove_carriage
+one.move_previous
+one.current_station
+stans.add_train(one)
+stans.add_train(two)
+stans.add_train(three)
+stans.by_type('passenger')
+stans.by_type('cargo')
+stans.trains.size
