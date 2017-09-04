31680c66df448ce75b5cc7c22363d7654ba534b8
delosiliana <dronixa@mail.ru>
2017-09-04 23:44:01 +0300

    Added seat carriage

--- lesson_7/lib/carriage_passenger.rb
+++ lesson_7/lib/carriage_passenger.rb
@@ -1,7 +1,18 @@
 class CarriagePassenger < Carriage
 
-  def initialize
+  def initialize(number_seats)
+    @number_seats = number_seats.to_i
+    @busy_place = @number_seats
     super(:passenger)
+  end
+
+  def free_seat
+    raise "Все места заняты" if @busy_place.zero?
+    @busy_place -= 1
+  end
+
+  def taken_places
+    @number_seats - @busy_place
   end
 
 end
